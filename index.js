67f91a8ab7f5988a7e5956b516df8317ff5b22a7
Brian Logan <briansr.@Brians-MBP.fios-router.home>
2020-08-07 17:16:39 -0400

    Variable excercise

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let age = 38;
 console.log(age);
-let favcColor = "red";
+let favColor = "red";
 console.log(favColor);
 let favHorror = "The-Ring";
-console.log(horrorMovie);
+console.log(favHorror);
 let superHero = "hulk";
 console.log(superHero);
-let num = 5.5;
+
 let tallerThanFiveFive = true;
 
 console.log(tallerThanFiveFive);
