1711d2c9ae0ed4f45871f547c429f69ccaa53dfc
Brian Logan <briansr.@Brians-MBP.fios-router.home>
2020-08-07 17:06:31 -0400

    variable exercise

---
+++ index.js
@@ -0,0 +1,16 @@
+let age = 38;
+let color = red;
+let horrorMovie = The-Ring;
+let superHero = hulk;
+let num = 5.5;
+function height(num){
+    if (num > 5.5) {
+        return true;
+    }        
+}
+
+console.log(age);
+console.log(color);
+console.log(horrorMovie);
+console.log(superHero);
+console.log(num(5.8));
